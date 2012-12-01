481c20c439871c14481a72c2715f4a7873997204
Sadish Ravi <sadish.r@gmail.com>
2012-12-01 16:41:01 -0500

    fix mongo uri

--- module/place/places.js
+++ module/place/places.js
@@ -4,12 +4,8 @@ var server = mongo.Server,
     db = mongo.Db,
     BSON = mongo.BSONPure;
 
-/*var mongoUri = process.env.MONGOLAB_URI || 
-  process.env.MONGOHQ_URL || 
-  'mongodb://ds043917.mongolab.com:43917/geoquotient';*/
-
 var mongoUri = process.env.MONGOLAB_URI ||
-  'mongodb://heroku_app9665789:jbcsk18vavbost20eh95bbf6qs@ds043987.mongolab.com:43987/heroku_app9665789'; 
+  'mongodb://heroku_app9665789:6f3tmtg733hdtmuh4dehjott9@ds043917.mongolab.com:43917/heroku_app9665789'; 
 
 console.log("mongoURI: " + mongoUri);
 
@@ -22,10 +18,11 @@ db.connect(mongoUri, function (err, db) {
                 console.log("The 'places' collection doesn't exist.");
             }else{
                 console.log("The 'places' collection exist.");
+                
+                collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+                });
             }
             
-            collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
-            });
         });
     }else{
         console.log("Failed to Connect to 'heroku_app9665789' database");
