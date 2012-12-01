807d96f838f5dc3974eb6cd3b31c82a509ca2589
Sadish Ravi <sadish.r@gmail.com>
2012-12-01 13:15:14 -0500

    changing server to conform to express 3.x

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var app = express();
+
+app.configure(function () {
+    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.bodyParser());
+});
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Welcome to GeoQuotient! How good is your GeoQuotient?');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log("GeoQuotient Server Listening on " + port);
 });
